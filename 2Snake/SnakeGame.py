d26d5ae967524b64f15fa6b39d2007359498114e
heerou <jalvarezpimiento@gmail.com>
2020-06-16 14:39:00 -0500

    Starting snake game

---
+++ 2Snake/SnakeGame.py
@@ -0,0 +1,52 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+class Cube(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class Snake(object):
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def drawGrid(w, rows, surface):
+    pass
+
+
+def redrawWiindow(w, rows, surface):
+    pass
+
+
+def randomSnack(w, rows, surface):
+    pass
+
+
+def main():
+    pass
