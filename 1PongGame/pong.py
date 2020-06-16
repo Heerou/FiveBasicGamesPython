5e4e2045cb0016145cb46e2dda0be04da634e237
heerou <jalvarezpimiento@gmail.com>
2020-06-16 12:36:16 -0500

    Now bounces in the screen and with the pallets

--- 1PongGame/pong.py
+++ 1PongGame/pong.py
@@ -80,7 +80,7 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    # Boder Checking
+    # Border Checking
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
@@ -90,9 +90,20 @@ while True:
         ball.dy *= -1
 
     if ball.xcor() > 390:
-        ball.setx(390)
+        ball.goto(0, 0)
         ball.dx *= -1
 
     if ball.xcor() < -390:
-        ball.setx(-390)
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and Ball collision
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (
+            ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (
+            ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
         ball.dx *= -1
