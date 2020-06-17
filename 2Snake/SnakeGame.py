94227ff78c398635e92a4125caee9e8c75725f1a
heerou <jalvarezpimiento@gmail.com>
2020-06-16 21:04:26 -0500

    Updated Snake project libreries and draws a grid and opens a pygame window

--- 2Snake/SnakeGame.py
+++ 2Snake/SnakeGame.py
@@ -37,11 +37,23 @@ class Snake(object):
 
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
 
 
-def redrawWiindow(w, rows, surface):
-    pass
+def redrawWindow(surface):
+    global rows, width
+    surface.fill((0, 0, 0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
 
 
 def randomSnack(w, rows, surface):
@@ -49,4 +61,21 @@ def randomSnack(w, rows, surface):
 
 
 def main():
+    global width, rows
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    mySnake = Snake((255, 0, 0), (10, 10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(win)
+
     pass
+
+
+main()
